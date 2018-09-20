3c282a8c8046ce71aafeb984e646b9128756445c
atouil <atouil@exoplatform.com>
2018-09-20 12:38:16 +0100

    new service

--- services/src/main/java/org/exoplatform/addons/MyService.java
+++ services/src/main/java/org/exoplatform/addons/MyService.java
@@ -1,4 +1,7 @@
 package org.exoplatform.addons;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.log.ExoLogger;
 import org.picocontainer.Startable;
 import org.exoplatform.services.log.Log;
@@ -12,12 +15,13 @@ public class MyService implements Startable{
 
     private static final Log LOG = ExoLogger.getLogger(MyService.class);
 
+    ExoContainer  currentContainer = ExoContainerContext.getCurrentContainer();
+        Myser  myser  =  (Myser)   currentContainer.getComponentInstanceOfType(Myser.class);
 
     public void start(){
 
         LOG.info(" service Started");
-
-
+        myser.afficher();
     }
     public void stop(){
         LOG.info("service stopped");
---
+++ services/src/main/java/org/exoplatform/addons/Myser.java
@@ -0,0 +1,17 @@
+package org.exoplatform.addons;
+
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+public class Myser {
+
+    private static final Log LOG = ExoLogger.getLogger(MyService.class);
+
+
+
+    public void afficher(){
+
+        LOG.info(" ** new service Started **");
+}
+
+}
